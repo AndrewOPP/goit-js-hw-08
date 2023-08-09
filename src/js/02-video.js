f0c52698cb970bec1c9f849e2828b6091981789c
AndrewOPP <a.vyblov2003@gmail.com>
2023-08-10 02:06:08 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,15 @@
 import Vimeo from '@vimeo/player'
-       
+import throttle from "lodash.throttle"
+     
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-
-player.on('play', function() {
-    console.log('played the video!');
-});
-
-const onPlay = function(data) {
-    console.log( data.seconds);
-       
-};
-
+const onPlay = throttle((data) => {
+    console.log(data.seconds);
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+}, 1000)  
 player.on('timeupdate', onPlay);
 
-    
+const currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"))
+
+player.setCurrentTime(currentTime);
